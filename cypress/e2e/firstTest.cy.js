e68fea7a918c6f8761738a134c730f8d53ff0ea1
hfridland <hfridland@shaw.ca>
2022-09-21 15:25:28 -0700

    Commit

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,2 +1,44 @@
 /// <reference types="cypress" />
 
+it('SHOULD', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('[data-qa-node="amount"]')
+        .clear().type(100)
+        .should('have.value', 100)
+        .and('be.visible')
+})
+
+it('Expect', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('[data-qa-node="amount"]')
+        .clear().type(100).then(input => {
+            expect(input).to.have.value(100)
+        })
+})
+
+it('Check default value for Deposits', () => {
+    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
+    cy.get('[data-qa-value="UAH"]')
+        .should('be.checked')
+})
+
+it('Check is visible Archive link', () => {
+    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
+    cy.contains('Мої депозити')
+        .trigger('mouseover')
+        .get('#archiveDeposits')
+        .should('be.visible')
+})
+
+it('Check correct attr in button', () => {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.contains('Show cards')
+        .should('have.attr', 'type')
+        .and('match', /button/)
+})
+
+it.only('Check is correct URL', () => {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.url()
+        .should('eq', 'https://next.privat24.ua/?lang=en')
+})
